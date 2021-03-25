c16d88fce3d00e920db51d17926794c168ac1ded
tonyrpark <tonyrpark@gmail.com>
2021-03-24 21:12:52 -0700

    fixing heroku still...

--- server.js
+++ server.js
@@ -35,6 +35,7 @@ app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(favicon(__dirname + /public/favicon.ico'));
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
