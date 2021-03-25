0d27e3d95e3f78540b459426de071637f67731c2
tonyrpark <tonyrpark@gmail.com>
2021-03-25 11:23:35 -0700

    updated server.js

--- server.js
+++ server.js
@@ -35,7 +35,6 @@ app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
-app.use(favicon(__dirname + /public/favicon.ico'));
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
