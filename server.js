b4a9fc7a4d6361ab112f9d9e57e60efc9da6eba2
tonyrpark <tonyrpark@gmail.com>
2021-03-24 20:45:18 -0700

    fixing heroku..

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ const hbs = exphbs.create({ helpers });
 const session = require("express-session");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || "8080";
+app.set("port", PORT);
 
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
